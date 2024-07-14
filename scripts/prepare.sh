031d364de37640059fe384fdaa705c3d67a7e40c
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-07-14 12:40:26 +0100

    Initial Version

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -6,6 +6,9 @@ SUBPROJECT_DIR=$(dirname $SCRIPT_DIR)
 PROJECT_DIR=$(dirname $SUBPROJECT_DIR)
 BUILD_DIR=${SUBPROJECT_DIR}/build
 
+pwd
+ls -al 
+tree ${PROJECT_DIR}
 
 ${PROJECT_DIR}/diaries-request/scripts/prepare.sh
 ${PROJECT_DIR}/diaries-response/scripts/prepare.sh
