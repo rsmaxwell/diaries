7f0237942110d0dc3d2c76c93ca6117ddbab2f53
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-07-14 13:11:27 +0100

    Initial Version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+set -x
+
 BASEDIR=$(dirname "$0")
 SCRIPT_DIR=$(cd $BASEDIR && pwd)
 SUBPROJECT_DIR=$(dirname $SCRIPT_DIR)
 PROJECT_DIR=$(dirname $SUBPROJECT_DIR)
 BUILD_DIR=${SUBPROJECT_DIR}/build
 
-
+pwd
+ls -al 
+tree
 
 cd ${SUBPROJECT_DIR}
 
