f68fa198d0e94def03457b218cbb1034e1480624
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-07-14 13:17:46 +0100

    Initial Version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
-set -x
-
 BASEDIR=$(dirname "$0")
 SCRIPT_DIR=$(cd $BASEDIR && pwd)
 PROJECT_DIR=$(dirname $SCRIPT_DIR)
 
-pwd
-ls -al 
-
-
 cd ${PROJECT_DIR}
-
-pwd
-ls -al 
 
 
 ${PROJECT_DIR}/gradlew publish \
