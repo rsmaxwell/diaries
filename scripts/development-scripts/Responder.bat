521164069e7b357a8e43da21d129796bc8848cf8
Richard Maxwell <richard@rsmaxwell.co.uk>
2026-02-14 19:57:44 +0000

    Fix CLASSPATH length problem in responder.bat

--- scripts/development-scripts/Responder.bat
+++ scripts/development-scripts/Responder.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 setLocal EnableDelayedExpansion
 
 set BASEDIR=%~dp0
@@ -26,12 +26,25 @@ cd %PROJECT_DIR%
 
 set CLASSPATH="%SUBPROJECT_DIR%\bin\main
 set CLASSPATH=%CLASSPATH%;%SUBPROJECT_DIR%\src\main\resources\META-INF
+set CLASSPATH=%CLASSPATH%;%COMMON_SUBPROJECT_DIR%\bin\main
 for /R %SUBPROJECT_DIR%\runtime %%a in (*.jar) do (
   set CLASSPATH=!CLASSPATH!;%%a
 )
 set CLASSPATH=%CLASSPATH%"
 
-set LOGGER_LEVEL=DEBUG
+set HIBERNATE_LOGLEVEL=OFF
+set LOGLEVEL=DEBUG
+
+rem --- pretty-print classpath, one entry per line ---
+set "CP=%CLASSPATH%"
+set "CP=%CP:"=%"   rem remove the leading/trailing quotes
+
+echo(
+echo ==== CLASSPATH entries ====
+for %%I in ("%CP:;=" "%") do echo %%~I
+echo ===========================
+echo(
+
 java -classpath %CLASSPATH% com.rsmaxwell.diaries.responder.Responder ^
  --config %USERPROFILE%\.diaries\responder.json
 
