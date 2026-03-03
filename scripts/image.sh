510f774bcbefa3caaf106631724fc6c2de655357
Richard Maxwell <richard@rsmaxwell.co.uk>
2026-03-03 16:42:41 +0000

    add the diaries-stack

---
+++ scripts/image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+SCRIPT_DIR=$(cd $BASEDIR && pwd)
+PROJECT_DIR=$(dirname $SCRIPT_DIR)
+BUILD_DIR=${PROJECT_DIR}/build
+
+. ${BUILD_DIR}/buildinfo
+
+
+cd ${PROJECT_DIR}
+
+
+
+echo "Hello World"
