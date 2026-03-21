0cc593dc8fb9616b8e8dbf5d005dcc26ba7fef83
Richard Maxwell <richard@rsmaxwell.co.uk>
2026-03-21 15:26:42 +0000

    add debug output

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,6 +15,9 @@ cd ${PROJECT_DIR}
 echo "GRADLE_USER_HOME=$GRADLE_USER_HOME"
 ls -al "$GRADLE_USER_HOME" || true
 
+ls -al /home/gradle/.gradle
+sed -n '1,20p' /home/gradle/.gradle/gradle.properties
+
 ${PROJECT_DIR}/gradlew publish --info --stacktrace \
     -PrepositoryName=${REPOSITORY} \
     -PprojectVersion=${VERSION}
